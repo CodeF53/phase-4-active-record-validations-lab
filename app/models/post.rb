6865090cd8629c492b7201b3f6a1eb40e1d24d9a
ChaseWelsh <fseusb@gmail.com>
2022-10-12 11:19:04 -0600

    commit tree 5af1b25b398cbf43c0eda70c17f7e1a96ecac1b7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :title_clickbait
+
+  def title_clickbait
+    errors.add(:title, 'needs clickbait') unless /(Won't Believe|Secret|Top \d+|Guess)/i.match title
+  end
 end
